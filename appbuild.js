23971c2476bc4823bc6ccb212370c21ebbf899f1
Ben <btaylor280@gmail.com>
2021-01-30 21:03:13 -0600

    Update of server

--- appbuild.js
+++ appbuild.js
@@ -1,15 +1,13 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var credentials = {key: privateKey, cert: certificate};
-var express = require('express');
-var app = express();
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
 
 var privateKey  = fs.readFileSync('/etc/letsencrypt/live/benjamintaylor.me/privkey.pem', 'utf8');
 var certificate = fs.readFileSync('/etc/letsencrypt/live/benjamintaylor.me/fullchain.pem', 'utf8');
 
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -22,8 +20,15 @@ app.get('/file', function (req, res) {
   res.download(__dirname + '/Resume.pdf', 'Resume.pdf');
 });
 
+//404 page
+app.get('*', function(req, res){
+  res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+});
+
+
+var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(443);
-//Http to Https
+
 var http_server = http.createServer(function(req,res){
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
